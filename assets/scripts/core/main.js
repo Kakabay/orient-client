e469681644f58373e69a333fa304e73d5144b59f
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-10-14 23:28:06 +0500

    nav

---
+++ assets/scripts/core/main.js
@@ -0,0 +1,16 @@
+// const swiperOptions = {
+//   freeMode: true,
+//   spaceBetween: 0,
+//   grabCursor: true,
+//   slidesPerView: 3,
+//   loop: true,
+//   autoplay: {
+//     delay: 1,
+//     disableOnInteraction: true,
+//   },
+//   freeMode: true,
+//   speed: 5000,
+//   freeModeMomentum: false,
+// };
+
+const swiper = new Swiper(".mySwiper", {});
