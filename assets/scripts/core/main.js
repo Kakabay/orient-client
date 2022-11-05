f1d35cb14056cc3886674110d9fa7201eb56075c
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-11-05 18:13:48 +0500

    burger - language

--- assets/scripts/core/main.js
+++ assets/scripts/core/main.js
@@ -92,6 +92,60 @@ class AssignYear {
   }
 }
 
+class AssignEvent {
+  identifier;
+  eventType;
+  actionType;
+  addedClass = "active";
+  target = "self";
+  /**
+   * ID or class of an HTML element
+   * @param {string} identifier
+   * Event type on which action is executed
+   * @param {string} eventType
+   * Type of action
+   * @param {"add" | "remove" | "toggle"} actionType
+   * Added classname
+   * @param {string} addedClass
+   * Target element identifier
+   * @param {string} target
+   *
+   */
+  constructor(identifier, eventType, actionType, addedClass, target) {
+    this.identifier = identifier;
+    this.eventType = eventType;
+    this.actionType = actionType;
+    this.addedClass = addedClass;
+    this.target = target;
+  }
+
+  listen() {
+    const element = new Select(this.identifier).select();
+    let target;
+    if (this.target === "self") {
+      target = element;
+    } else {
+      target = new Select(this.target).select();
+    }
+    element.addEventListener(this.eventType, () => {
+      switch (this.actionType) {
+        case "add":
+          target.classList.add(this.addedClass);
+          break;
+        case "remove":
+          target.classList.remove(this.addedClass);
+          break;
+        case "toggle":
+          target.classList.toggle(this.addedClass);
+          break;
+        default:
+          throw new Error("Bad action type!");
+      }
+    });
+    return [element, target];
+  }
+}
+
 // Year
 const displayedYear = new AssignYear("#year").assign();
 
